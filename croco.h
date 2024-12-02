7d17b7b724d5c7082e785cbf3fc470da9b3d0853
m.usai287 <m.usai287@studenti.unica.it>
2024-12-02 18:31:45 +0100

    deleted:    Makefile 	deleted:    croco.c 	deleted:    croco.h 	deleted:    croco.o 	deleted:    frog.c 	deleted:    frog.h 	deleted:    frog.o 	deleted:    frogger.c 	deleted:    frogger.o 	deleted:    funzionamento.c 	deleted:    funzionamento.h 	deleted:    funzionamento.o 	deleted:    piddini.c 	deleted:    piddini.h 	deleted:    piddini.o 	deleted:    struct.h

--- croco.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <ncurses.h>
-
-#define DELAY 1000000 //0.001
-#define DELAYCLOSED 2000000 //2 secondi
-
-void cocco(int pipe_fd);
--- funzionamento.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <ncurses.h>
-#include "struct.h"
-
-#include "frog.h"
-#include "croco.h"
-
-void funzionamento_gioco (int pipe_fd);
