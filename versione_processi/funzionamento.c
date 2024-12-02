8d015f8460733a50f2882bc251c29205932711fc
m.usai287 <m.usai287@studenti.unica.it>
2024-12-02 18:27:45 +0100

    aggiunta versione processi

---
+++ versione_processi/funzionamento.c
@@ -0,0 +1,44 @@
+#include   <ncurses.h>
+#include 	<unistd.h>
+#include 	<stdlib.h>
+#include 	  <time.h>
+#include  <sys/wait.h>
+#include <sys/types.h>
+#include 	<signal.h>
+
+#include 		"struct.h"
+#include "funzionamento.h"
+#include 		  "frog.h"
+#include 		 "croco.h"
+
+void funzionamento_gioco (int pipe_fd){
+	MesPos message                         ;
+	MesPos rana	    ={0, LINES -1, COLS -1};
+	MesPos croco	={1,0,0}               ;
+
+	while(1){
+		if (read(pipe_fd, &message, sizeof(MesPos)) > 0) {
+			if (message.tipo == 0) {
+                rana = message;
+			}
+            else if (message.tipo == 1){
+                croco = message;
+            }
+			//questo controllo if  else if serve per capire chi sta usando il buffer
+			clear();
+            attron(COLOR_PAIR(1))            ;
+			mvprintw(rana.x, rana.y, "#")  ; //simbolo usato per rappresentare il personaggio
+		    attron(COLOR_PAIR(2))            ;
+            mvprintw(croco.x, croco.y, "$$$$"); //simbolo usato per rappresentare il personaggio
+			refresh();
+
+			if (rana.x == croco.x && rana.y == croco.y){
+				mvprintw(LINES / 2, COLS / 2 -5, "preso");
+				refresh()                                ;
+				usleep(DELAYCLOSED)                      ;
+				endwin()                                 ;
+				exit(0)                                  ;
+			} //win condition
+		}
+	}
+}
---
+++ versione_processi/piddini.c
@@ -0,0 +1,50 @@
+#include <ncurses.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#include "piddini.h"
+
+
+
+void creaPipe(int pipe_fd[2]) {
+    if (pipe(pipe_fd) == -1) {
+        perror("Errore nella creazione della pipe");
+        exit(1);
+    }
+}
+
+void creaRano(int pipe_fd[2], pid_t *pid_rana) {
+    *pid_rana = fork();
+    if (*pid_rana == -1) {
+        perror("Errore nella creazione della prima fork");
+        _exit(1);
+    }
+
+    if (*pid_rana == 0) {  // Processo figlio (rana)
+        close(pipe_fd[0]);  // Chiudi il lato di lettura della pipe
+        // Esegui altre operazioni nel processo figlio 'rana'
+        rano(pipe_fd[1], "Rana", 4);  // Esempio di scrittura nella pipe
+        close(pipe_fd[1]);  // Chiudi il lato di scrittura della pipe
+        _exit(0);  // Esci dal processo figlio
+    }
+}
+
+void creaCroco(int pipe_fd[2], pid_t *pid_croco) {
+    *pid_croco = fork();
+    if (*pid_croco == -1) {
+        perror("Errore nella creazione della seconda fork");
+        _exit(1);
+    }
+
+    if (*pid_croco == 0) {  // Processo figlio (coccodrillo)
+        close(pipe_fd[0]);  // Chiudi il lato di lettura della pipe
+        // Esegui altre operazioni nel processo figlio 'coccodrillo'
+        cocco(pipe_fd[1], "Croco", 5);  // Esempio di scrittura nella pipe
+        close(pipe_fd[1]);  // Chiudi il lato di scrittura della pipe
+        _exit(0);  // Esci dal processo figlio
+    }
+}
